04625e38f65481ed7cdaaec69583a6d5e3c1a40f
idkreally989 <shreyashsharma9314@gmail.com>
2022-06-15 01:08:38 +0530

    slider is now working and dynamic

--- javascript.js
+++ javascript.js
@@ -1,14 +1,25 @@
-let gridNum = 5;
-let gridSize = 800/gridNum;
+let slider = document.getElementById("dimensions");
+let gridNum = slider.value;
+console.log(gridNum);
 
-for(let i = 0; i < gridNum*gridNum; i++){
-    let newdiv = document.createElement("div");
-    newdiv.className = "box";
-    newdiv.style.width = gridSize-2 + "px";
-    newdiv.style.height = gridSize-2 + "px";
-    newdiv.addEventListener("mouseover", function(){
-        newdiv.style.backgroundColor = "black";
-    });
-    document.getElementById("mainContainer").appendChild(newdiv);
+slider.oninput = function() {
+    gridNum = slider.value;
+    createGrids(gridNum);
+}
+function createGrids(gridNum){
+    let container = document.getElementById("mainContainer");
+    container.innerHTML = "";
+
+    let gridSize = 800/gridNum;
+    for(let i = 0; i < gridNum*gridNum; i++){
+        let newdiv = document.createElement("div");
+        newdiv.className = "box";
+        newdiv.style.width = gridSize-2 + "px";
+        newdiv.style.height = gridSize-2 + "px";
+        newdiv.addEventListener("mouseover", function(){
+            newdiv.style.backgroundColor = "black";
+        });
+        document.getElementById("mainContainer").appendChild(newdiv);
+    }
 }
 
