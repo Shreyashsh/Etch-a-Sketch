371cecd6c137e00195d0b8e962136e06eda59996
idkreally989 <shreyashsharma9314@gmail.com>
2022-06-15 00:56:00 +0530

    creates div and allows hover to color them black

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
+let gridNum = 5;
+let gridSize = 800/gridNum;
+
+for(let i = 0; i < gridNum*gridNum; i++){
+    let newdiv = document.createElement("div");
+    newdiv.className = "box";
+    newdiv.style.width = gridSize-2 + "px";
+    newdiv.style.height = gridSize-2 + "px";
+    newdiv.addEventListener("mouseover", function(){
+        newdiv.style.backgroundColor = "black";
+    });
+    document.getElementById("mainContainer").appendChild(newdiv);
+}
 
