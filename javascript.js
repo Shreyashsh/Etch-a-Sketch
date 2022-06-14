5b6749a6d2bac1d1241473126fc553af2f56b1e5
idkreally989 <shreyashsharma9314@gmail.com>
2022-06-15 01:31:03 +0530

    added option for rbg

--- javascript.js
+++ javascript.js
@@ -6,10 +6,38 @@ slider.oninput = function() {
     gridNum = slider.value;
     createGrids(gridNum);
 }
+
+let toRandom = document.getElementById("toRandom");
+let toBlack = document.getElementById("toBlack");
+
+toBlack.addEventListener("click", function(){
+    let divs = document.querySelectorAll(".box");
+    divs.forEach( div => {
+        div.addEventListener("mouseover", function(){
+            div.style.backgroundColor = "black";
+        })
+    })
+});
+
+toRandom.addEventListener("click", function(){
+    let divs = document.querySelectorAll(".box");
+    divs.forEach( div => {
+        div.addEventListener("mouseover", function(){
+            div.style.backgroundColor = randomRBG();
+        })
+    })
+});
+
+function randomRBG(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
 function createGrids(gridNum){
     let container = document.getElementById("mainContainer");
     container.innerHTML = "";
-
     let gridSize = 800/gridNum;
     for(let i = 0; i < gridNum*gridNum; i++){
         let newdiv = document.createElement("div");
